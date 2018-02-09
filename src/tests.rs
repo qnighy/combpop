fc4d99ed6143fe97e732c69083e6a646af29f512
Masaki Hara <ackie.h.gmai@gmail.com>
2018-02-09 15:31:51 +0900

    Add json_string as a test.

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,67 @@
 use super::*;
 use stream::SliceStream;
 
+pub mod json {
+    use ::*;
+    use stream::SliceStream;
+    parser! {
+        pub fn json_string() -> JsonString<Input = u8, Output = String> {
+            // let hexchar = || {
+            //     byte::char(|x| '0' <= x && x <= '9').map(|x| x as u32 - 0x30)
+            //         .or(byte::char(|x| 'A' <= x && x <= 'F').map(|x| x as u32 - 0x41 + 10))
+            //         .or(byte::char(|x| 'a' <= x && x <= 'f').map(|x| x as u32 - 0x61 + 10))
+            // };
+            // let u16escape = || {
+            //     byte::char(|x| x == 'u')
+            //     .concat(hexchar())
+            //     .concat(hexchar())
+            //     .concat(hexchar())
+            //     .concat(hexchar())
+            //     .map(|((((_, x0), x1), x2), x3)| (x0 << 12) | (x1 << 8) | (x2 << 4) | x3)
+            // };
+            // let unicode_escape = || {
+            //     u16escape().and_then(|x| {
+            //
+            //     })
+            // };
+            // TODO: \uXXXX
+            let escaped =
+                byte::char(|x| x == '"' || x == '\\' || x == '/')
+                .or(byte::char(|x| x == 'b').map(|_| '\x08'))
+                .or(byte::char(|x| x == 'f').map(|_| '\x0C'))
+                .or(byte::char(|x| x == 'n').map(|_| '\n'))
+                .or(byte::char(|x| x == 'r').map(|_| '\r'))
+                .or(byte::char(|x| x == 't').map(|_| '\t'));
+            let escaped = byte::char(|x| x == '\\').concat(escaped).map(|(_, x)| x);
+            // TODO: many instead of many1
+            byte::char(|x| x == '"')
+                .concat(escaped.or(byte::char(|x| x != '"')).many1().collect::<String>())
+                .concat(byte::char(|x| x == '"'))
+                .map(|((_, x), _)| x)
+        }
+    }
+
+    #[test]
+    fn test_json_string() {
+        let p = json_string();
+        assert_eq!(p.parse(&mut SliceStream::new(b"\"hoge\"")).unwrap(), "hoge");
+        assert_eq!(
+            p.parse(&mut SliceStream::new(b"\"ho\rge\"")).unwrap(),
+            "ho\rge"
+        );
+        assert_eq!(
+            p.parse(&mut SliceStream::new(b"\"ho\\rge\"")).unwrap(),
+            "ho\rge"
+        );
+        assert_eq!(
+            p.parse(&mut SliceStream::new(b"\"ho\\rge\" ")).unwrap(),
+            "ho\rge"
+        );
+        assert!(p.parse(&mut SliceStream::new(b"\"ho\\rge")).is_err());
+        assert!(p.parse(&mut SliceStream::new(b" \"hoge\"")).is_err());
+    }
+}
+
 parser! {
     pub fn ident() -> Ident<Input = u8, Output = String> {
         byte::alpha().many1().collect::<String>()
